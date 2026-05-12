36e910b2bdd384bec25612d3041859d04708db3a
Jacob Moore <jlmoors001@gmail.com>
2026-05-11 23:36:38 -0500

    Default Google Workspace OAuth to read-only

--- skills/productivity/google-workspace/scripts/google_api.py
+++ skills/productivity/google-workspace/scripts/google_api.py
@@ -44,13 +44,11 @@ CLIENT_SECRET_PATH = HERMES_HOME / "google_client_secret.json"
 
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
-    "https://www.googleapis.com/auth/gmail.send",
-    "https://www.googleapis.com/auth/gmail.modify",
-    "https://www.googleapis.com/auth/calendar",
-    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/drive.readonly",
     "https://www.googleapis.com/auth/contacts.readonly",
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://www.googleapis.com/auth/documents.readonly",
 ]
 
 
